5aaaa30ac1de3b14f12c873e0ba0b12c576a1785
Zi0LEO <leonardo.npl.02@gmail.com>
2024-06-12 11:54:10 +0200

    Implemented sleepy barber problem

---
+++ src/ClassicProblems/SleepingBarber/Customer.java
@@ -0,0 +1,28 @@
+package ClassicProblems.SleepingBarber;
+
+public class Customer implements Runnable{
+
+    private final BarberHall hall;
+    private final int id;
+
+    public Customer(BarberHall hall, int id){
+        this.hall = hall;
+        this.id = id;
+    }
+
+    @Override
+    public void run() {
+        while(true){
+            try{
+                boolean canCut = hall.waitForCut();
+                if(canCut)
+                    System.out.format("Customer %d managed to cut his hair \n", id);
+                else
+                    System.out.format("Customer %d went away \n", id);
+            } catch (InterruptedException ie) {
+                System.out.println("Exception in customer " + id);
+            }
+        }
+
+    }
+}
