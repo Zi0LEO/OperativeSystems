d5b052d7879e20d04b01f1fbb3dd1ea580dfaf9a
Zi0LEO <leonardo.npl.02@gmail.com>
2024-06-18 18:42:57 +0200

    Semaphores implementation

---
+++ src/EserciziEsame/AziendaAgricola/AziendaAgricolaSemafori.java
@@ -0,0 +1,40 @@
+package EserciziEsame.AziendaAgricola;
+
+import java.util.concurrent.Semaphore;
+
+public class AziendaAgricolaSemafori extends AziendaAgricola{
+
+    protected Semaphore magazzinoVuoto = new Semaphore(0, true);
+    protected Semaphore sacchiDisponibili = new Semaphore(MAX_CAPACITA,true);
+    protected Semaphore mutexCassa = new Semaphore(1, true);
+
+    @Override
+    protected void pagaSacchi(int sacchi) {
+        try {
+            mutexCassa.acquire();
+            incasso += PREZZO_SACCO * sacchi;
+
+            mutexCassa.release();
+        } catch (InterruptedException ignored) {}
+        }
+
+    @Override
+    protected void prendiSacchi(int sacchi) {
+        try {
+            for (int i = 0; i < sacchi; i++) {
+                sacchiDisponibili.acquire();
+                magazzinoVuoto.release();
+                spostaSacco();
+            }
+        }catch (InterruptedException ignored){}
+    }
+
+    @Override
+    protected void resettaMagazzino() {
+        try {
+            magazzinoVuoto.acquire(MAX_CAPACITA);
+            riempiMagazzino();
+        }catch (InterruptedException ignored){}
+        sacchiDisponibili.release(MAX_CAPACITA);
+    }
+}
